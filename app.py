6fe5622fc57ca27823bf3cb1134aab96775f1dc4
Nachomen3322 <nachomen3322@gmail..com>
2024-02-21 15:41:16 -0400

    cambio1

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    try:
+        server_address = ('', 8000)
+        httpd = server_class(server_address, handler_class)
+        print('Iniciando servidor web en localhost')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Apagando la malcriada')
+        httpd.socket.close()
+
+if __name__ == "__main__":
+    run()
